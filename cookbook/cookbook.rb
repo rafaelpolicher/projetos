caf6153eafab1737b225ccdc78fd742f727e4ec8
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-02-15 21:58:54 -0300

    Update cookbook.rb

--- cookbook/cookbook.rb
+++ cookbook/cookbook.rb
@@ -1,35 +1,52 @@
-puts "Bem vindo ao COOKBOOK, sua rede social de receitas "
+
+
+def bem_vindo
+    puts "Bem vindo ao COOKBOOK, sua rede social de receitas "
+end
+
+def menu
+    puts "[1] Cadastrar receita"
+    puts "[2] Ver receitas"
+    puts "[3] sair"
+    print "Escolha uma opçao: "
+    return gets.to_i()
+end
+
+def inserir_receita
+    puts "Digite o nome da receita: "
+    nome = gets.chomp()
+
+     puts "Digite o tipo da receita: "
+    tipo = gets.chomp()
+
+    puts
+
+    puts
+    puts "Receita #{nome} cadastrada com sucesso"
+    puts
+    return receitas << {nome: nome, tipo: tipo}
+end
+
+def imprimir_receitas(r)
+    puts "====Receitas cadastradas==="
+    r.each do |receita|
+       puts "#{receita[:nome]} - #{receita[:tipo]}"
+end
+bem_vindo
 
 receitas = []
-puts "[1] Cadastrar receita"
-puts "[2] Ver receitas"
-puts "[3] sair"
 
-print "Escolha uma opçao: "
+opcao = menu()
+
 opcao = gets.to_i()
 while(opcao != 3)
 
     if(opcao ==1)
-        puts "Digite o nome da receita: "
-        nome = gets.chomp()
-
-        puts "Digite o nome da receita: "
-        tipo = gets.chomp()
-
-        puts
-        receitas << {nome: nome, tipo: tipo}
-        puts
-
-        puts
-        puts "Receita #{nome} cadastrada com sucesso"
-        puts
+    receitas << inserir_receita()
     
 
     elsif(opcao == 2)
-        puts "====Receitas cadastradas==="
-        receitas.each do |receita|
-           puts "#{receita[:nome]} - #{receita[:tipo]}"
-        end
+        imprimir_receitas(receitas)
         #########igual#############
         #receitas.each do |receita|
         #    puts receita
@@ -43,12 +60,8 @@ while(opcao != 3)
     else
         puts "Escolha uma opçao valida"
     end
-        puts "[1] Cadastrar receita"
-        puts "[2] Ver receitas"
-        puts "[3] sair"
 
-        print "Escolha uma opçao: "
-        opcao = gets.to_i()
+    opcao = menu()
 end
 
 puts "obrigado por usar o cookbook"
